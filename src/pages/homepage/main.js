7ff1a7315d29b681342bd5671fd85eac8efc5121
as_mih <michelledearaujo2@gmail.com>
2022-11-28 17:49:32 -0300

    criação de template dos produtos
    Co-authored-by: Anna Clara Andrade <andradeannac@users.noreply.github.com>

---
+++ src/pages/homepage/main.js
@@ -0,0 +1,30 @@
+export default () => {
+    const container = document.createElement('div');
+  
+    const templateProducts = `
+        <header>
+            <nav>
+                <img src='' alt='Logo do CitiBank, com as palavras Citi escrito em letras brancas e 
+                    um arco vermelho em cima da palavra>
+                <a href='#cart'/>
+                <a href='#login'> Faça login ou cadastre-se</a>
+            </nav>
+        </header>
+        <body>
+            <main>
+                <nav>
+                    <ol>
+                        <li>Todos os Produtos</li>
+                        <li>Mouse</li>
+                        <li>Teclado</li>
+                        <li>Headset</li>
+                        <li>Webcam</li>
+                        <li>Mousepad</li>
+
+
+  `;
+
+  container.innerHTML = templateProducts;
+  return container;
+
+}

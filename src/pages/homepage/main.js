c579e4dc8dccc7bc82c64d6692bcab46d5df20c6
as_mih <michelledearaujo2@gmail.com>
2022-11-28 18:24:04 -0300

    adicionando imagens

--- src/pages/homepage/main.js
+++ src/pages/homepage/main.js
@@ -4,23 +4,30 @@ export default () => {
     const templateProducts = `
         <header>
             <nav>
-                <img src='' alt='Logo do CitiBank, com as palavras Citi escrito em letras brancas e 
-                    um arco vermelho em cima da palavra>
-                <a href='#cart'/>
-                <a href='#login'> Faça login ou cadastre-se</a>
+                <img src='assets/logo-citi.png' alt='Logo do CitiBank, com a palavra Citi escrito em letras brancas e 
+                    um arco vermelho em cima da palavra'>
+                <a href='#login'><img src='assets/login.png' alt='Botão branco de redirecionamento à página de login'>Faça login ou cadastre-se</a>
+                <a href='#cart'><img src='assets/carrinho.png' alt='Botão branco de redirecionamento ao carrinho'></a>
             </nav>
         </header>
         <body>
             <main>
                 <nav>
-                    <ol>
+                    <ul>
                         <li>Todos os Produtos</li>
                         <li>Mouse</li>
                         <li>Teclado</li>
                         <li>Headset</li>
                         <li>Webcam</li>
                         <li>Mousepad</li>
-                        
+                    </ul>    
+                </nav>
+                
+                <div class=cards-products>
+
+                </div>
+
+            </main>
         </body>
 
   `;

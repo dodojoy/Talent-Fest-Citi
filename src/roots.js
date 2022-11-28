6a65c31c3280070a79d395d3c796e610d32cbc08
as_mih <michelledearaujo2@gmail.com>
2022-11-28 18:40:00 -0300

    criação da estrutura da pagina de produtos

--- src/roots.js
+++ src/roots.js
@@ -1,6 +1,6 @@
-// import homepage from './pages/homepage/main.js';
+import homepage from './pages/homepage/main.js';
 // import cart from './pages/cart/main.js';
-import login from './pages/login/main.js';
+// import login from './pages/login/main.js';
 // import register from './pages/register/main.js';
 // import qrcode from './pages/qrcode/main.js';
 // import products from './pages/products/main.js';
@@ -10,26 +10,26 @@ const main = document.querySelector('#root');
 function loadRoots() {
   main.innerHTML = '';
   switch (window.location.hash) {
-    // case '':
-    //   main.appendChild(homepage());
-    //   break;
-    case '#login':
-      main.appendChild(login());
+    case '':
+      main.appendChild(homepage());
       break;
-  //   case '#register':
-  //     main.appendChild(register());
-  //     break;
-  //   case '#products':
-  //     main.appendChild(products());
-  //     break;
-  //   case '#cart':
-  //     main.appendChild(cart());
-  //     break;
-  //   case '#qrcode':
-  //     main.appendChild(qrcode());
-  //     break;
-  //   default:
-  //     main.appendChild(homepage());
+    // case '#login':
+    //   main.appendChild(login());
+    //   break;
+    // case '#register':
+    //   main.appendChild(register());
+    //   break;
+    // case '#products':
+    //   main.appendChild(products());
+    //   break;
+    // case '#cart':
+    //   main.appendChild(cart());
+    //   break;
+    // case '#qrcode':
+    //   main.appendChild(qrcode());
+    //   break;
+    default:
+      main.appendChild(homepage());
   }
 }
 

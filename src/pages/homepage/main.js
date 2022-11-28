10b7e220bab6ae1718720fb337d60bc76a46ec04
joyce <joyce.mbt@gmail.com>
2022-11-28 17:46:13 -0300

    exemplo de template

---
+++ src/pages/homepage/main.js
@@ -0,0 +1,8 @@
+export default () => {
+  const resetContainer = document.createElement('div');
+  const template = `
+            <div>funciona pf</div>
+          `;
+  resetContainer.innerHTML = template;
+  return resetContainer;
+};

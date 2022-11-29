67da3974eba0cc960ff1586511e5fcbd58cc4d6f
joyce <joyce.mbt@gmail.com>
2022-11-29 11:50:00 -0300

    iniciei a configuração do firebase storage

--- src/lib/firebase.config.js
+++ src/lib/firebase.config.js
@@ -1,13 +1,15 @@
-import { initializeApp } from './exports.js';
+import { getStorage } from 'firebase/storage';
+import { initializeApp } from 'firebase/app';
 
 const firebaseConfig = {
-    apiKey: "AIzaSyCaNoB6Z2c9Sng5aMoVDxRm8IX0lF6JKP4",
-    authDomain: "citi-shop-c955b.firebaseapp.com",
-    projectId: "citi-shop-c955b",
-    storageBucket: "citi-shop-c955b.appspot.com",
-    messagingSenderId: "851557419207",
-    appId: "1:851557419207:web:fa49470d3443b71f5f16e2",
-    measurementId: "G-K7NS6KPMBF"
-  };
+  apiKey: 'AIzaSyCaNoB6Z2c9Sng5aMoVDxRm8IX0lF6JKP4',
+  authDomain: 'citi-shop-c955b.firebaseapp.com',
+  projectId: 'citi-shop-c955b',
+  storageBucket: 'citi-shop-c955b.appspot.com',
+  messagingSenderId: '851557419207',
+  appId: '1:851557419207:web:fa49470d3443b71f5f16e2',
+  measurementId: 'G-K7NS6KPMBF',
+};
 
-  export const root = initializeApp(firebaseConfig);
+export const app = initializeApp(firebaseConfig);
+export const storage = getStorage(app);

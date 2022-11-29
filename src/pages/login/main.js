dc50560d9c3fa0c55ee92076745c94334cb07742
Polyanagm <polyana.gm@hotmail.com>
2022-11-29 12:02:04 -0300

    css-login

--- src/pages/login/main.js
+++ src/pages/login/main.js
@@ -4,7 +4,7 @@ export default () => {
 
     const template = `
     <img class="logo" src="assets/logoazul.png">
-    <h1 class="bem-vindo">Bem vindo ao Citi Shop</h1>
+    <h1 class="start">Bem vindo ao Citi Shop</h1>
     <form class="form-input">
       <input class="input-email" type="email" id="email" placeholder="E-MAIL">
       <input class="input-password" type="password" id="password" placeholder="SENHA">
@@ -13,10 +13,10 @@ export default () => {
         <img id="img-google" src="assets/btn_google_signin_light_normal_web@2x.png" alt="botão de login com conta google">
       </button>
     </form>
-    <p id="reset-senha">Esqueceu sua senha?</p>
+    <p id="reset-password">Esqueceu sua senha?</p>
     <p class="msg-error"></p>
     <footer class="footer">
-      <h4 class="conta">Não possui uma conta?</h4>
+      <h4 class="forget">Não possui uma conta?</h4>
       <a id="btn-register" href="#register">Cadastre-se!</a>
     </footer>
             `;

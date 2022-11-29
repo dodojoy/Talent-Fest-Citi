d8059557fa4675e5e9da6e31fc4b21f0139f1fce
andradeannac <andradeannac@gmail.com>
2022-11-29 15:12:56 -0300

    resposividade homepage menu

--- src/pages/homepage/main.js
+++ src/pages/homepage/main.js
@@ -5,6 +5,9 @@ export default () => {
       <body>
           <header>
             <nav class='nav-bar-homepage'>
+                <div class="div-menu">
+                    <button id="btnMenu" class="btn-menu"><img src="assets/menu.png"</button>
+                </div>
                 <img class='img-logo-nav' src='assets/logo-citi.png' alt='Logo do CitiBank, com a palavra Citi escrito em letras brancas e 
                         um arco vermelho em cima da palavra'>
                 <div>
@@ -15,7 +18,7 @@ export default () => {
           </header>
 
           <main>
-            <nav class='nav-filter-homepage'>
+            <nav class='nav-filter-homepage' id='navFilter'>
                 <ul id='menu' class='menu'>
                     <li class='list-allproducts'>Todos os Produtos</li>
                     <details>
@@ -34,20 +37,28 @@ export default () => {
           </main>
 
           <footer class='footer-homepage'>
-            <div class='footer-logo-copyright'>
-                <img src='assets/logo-citi.png' alt='Logo do CitiBank, com a palavra Citi escrito em letras brancas e 
-                um arco vermelho em cima da palavra'>
-                <p>© 2022 Citigroup Inc. <br> Todos os direitos reservados.</p>
-            </div>
+          <div class='footer-logo-links'>
+            <img src='assets/logo-citi.png' alt='Logo do CitiBank, com a palavra Citi escrito em letras brancas e um arco vermelho em cima da palavra'>
             <a href='#developers'>Desenvolvedoras</a>
             <a href='https://corporateportal.brazil.citibank.com/quem-somos.htm' target='_blank'>Sobre o Cit</a>
+          </div>
+          <div class='footer-copyright'>
+            <p>© 2022 Citigroup Inc. Todos os direitos reservados.</p>
+          </div>
           </footer>
       </body>
 
 
 `;
-
 container.innerHTML = templateProducts;
+
+const menu = container.querySelector('#btnMenu')
+
+menu.addEventListener('click', () => {
+    const navFilter = container.querySelector('#navFilter')
+    navFilter.classList.toggle('active');
+  });
+
 return container;
 
 }

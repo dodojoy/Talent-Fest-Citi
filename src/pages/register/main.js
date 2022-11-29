7d05644a8390ebe43009e10244a929924f961fba
Polyanagm <polyana.gm@hotmail.com>
2022-11-29 17:10:43 -0300

    Criei as funções de login e register

--- src/pages/register/main.js
+++ src/pages/register/main.js
@@ -1,3 +1,11 @@
+import {
+  registerWithEmailAndPassword,
+} from '../../lib/firebase-auth.js';
+
+import {
+  validateRegisterForm,
+} from '../../lib/validation.js';
+
 export default () => {
     const resetContainer = document.createElement('div');
     resetContainer.setAttribute('class', 'container');
@@ -6,10 +14,10 @@ export default () => {
     <img class="logo" src="assets/logoazul.png">
     <h1 class="start">Bem vindo ao Citi Shop</h1>
     <form class="form-input">
-      <input class="input-name" type="password" id="password" placeholder="NOME">
-      <input class="input-email" type="email" id="email" placeholder="E-MAIL">
-      <input class="input-password" type="password" id="password" placeholder="SENHA">
-      <input class="input-password-confirm" type="password" id="password" placeholder="CONFIRME SUA SENHA">
+      <input class="input-name" type="password" id="input-name" placeholder="NOME">
+      <input class="input-email" type="email" id="input-email" placeholder="E-MAIL">
+      <input class="input-password" type="password" id="input-password" placeholder="SENHA">
+      <input class="input-password-confirm" type="password" id="input-password-confirm" placeholder="CONFIRME SUA SENHA">
       <input id="btn-register" type="submit" value="CADASTRAR" />
     </form>
     <p class="msg-error"></p>
@@ -19,5 +27,40 @@ export default () => {
     </footer>
             `;
     resetContainer.innerHTML = template;
-    return resetContainer;
+
+    const name = resetContainer.querySelector('#input-name');
+    const email = resetContainer.querySelector('#input-email');
+    const password = resetContainer.querySelector('#input-password');
+    const confirmPassword = resetContainer.querySelector('#input-password-confirm');
+    const form = resetContainer.querySelector('.form-input');
+    const loginBtn = resetContainer.querySelector('#btn-pag-login');
+
+    form.addEventListener('submit', (e) => {
+      e.preventDefault();
+      const formValidation = validateRegisterForm(
+        name.value,
+        email.value,
+        password.value,
+        confirmPassword.value,
+      );
+      if (formValidation) {
+        formValidationMessages.classList.remove('hide');
+        formValidationMessages.innerHTML = formValidation;
+      } else {
+        registerWithEmailAndPassword(name.value, email.value, password.value)
+          .then(() => {
+            window.location.hash = '#homepage';
+          })
+          .catch((error) => {
+            handleErrors(error);
+          });
+      }
+    });
+
+    loginBtn.addEventListener('click', () => {
+      window.location.hash = '#homepage';
+      
+    });
+
+ return resetContainer;
 };

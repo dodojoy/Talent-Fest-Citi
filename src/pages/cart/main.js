b79927660c71dd96ece86d25d6085115c47dc2ee
Polyanagm <polyana.gm@hotmail.com>
2022-11-30 19:24:20 -0300

    criação página desenvolvedoras

--- src/pages/cart/main.js
+++ src/pages/cart/main.js
@@ -16,57 +16,57 @@ export default () => {
                 <main>
                 <section id='cards-products' class='cards-products'> </section>
 
-                <section class="cards-andrea">
-                    <div class="filtro">
+                <section class='cards-andrea'>
+                    <div class='filtro'>
                         <h3>Andrea dos Santos</h3>
                         <p>Aluna da 8º geração da Laboratória</p>
-                        <a href="https://github.com/Canzua" target="_blank"><img src="assets/github.png"
-                                alt="github"></a>
-                        <a href="https://www.linkedin.com/in/andreasiqueiradossantos/" target="_blank"><img
-                                src="assets/linkedin.png" alt="linkedin"></a>
+                        <a href='https://github.com/Canzua' target='_blank'><img src='assets/github.png'
+                                alt='github'></a>
+                        <a href='https://www.linkedin.com/in/andreasiqueiradossantos/' target='_blank'><img
+                                src='assets/linkedin.png' alt='linkedin'></a>
                     </div>
                 </section>
 
-                <section class="cards-anna">
-                    <div class="filtro">
+                <section class='cards-anna'>
+                    <div class='filtro'>
                         <h3>Anna Clara Andrade</h3>
                         <p>Aluna da 8º geração da Laboratória</p>
-                        <a href="https://github.com/andradeannac" target="_blank"><img src="assets/github.png"
-                                alt="github"></a>
-                        <a href="https://www.linkedin.com/in/andradeannac/" target="_blank"><img
-                                src="assets/linkedin.png" alt="linkedin"></a>
+                        <a href='https://github.com/andradeannac' target='_blank'><img src='assets/github.png'
+                                alt='github'></a>
+                        <a href='https://www.linkedin.com/in/andradeannac/' target='_blank'><img
+                                src='assets/linkedin.png' alt='linkedin'></a>
                     </div>
                 </section>
                 
-                <section class="cards-joyce">
-                    <div class="filtro">
+                <section class='cards-joyce'>
+                    <div class='filtro'>
                         <h3>Joyce Teodoro</h3>
                         <p>Aluna da 8º geração da Laboratória</p>
-                        <a href="https://github.com/dodojoy" target="_blank"><img src="assets/github.png"
-                                alt="github"></a>
-                        <a href="https://www.linkedin.com/in/joyce-teodoro/" target="_blank"><img
-                                src="assets/linkedin.png" alt="linkedin"></a>
+                        <a href='https://github.com/dodojoy' target='_blank'><img src='assets/github.png'
+                                alt='github'></a>
+                        <a href='https://www.linkedin.com/in/joyce-teodoro/' target='_blank'><img
+                                src='assets/linkedin.png' alt='linkedin'></a>
                     </div>
                 </section>
 
-                <section class="cards-michele">
-                    <div class="filtro">
+                <section class='cards-michele'>
+                    <div class='filtro'>
                         <h3>Michele Araujo</h3>
                         <p>Aluna da 8º geração da Laboratória</p>
-                        <a href="https://github.com/asmih" target="_blank"><img src="assets/github.png"
-                                alt="github"></a>
-                        <a href="https://www.linkedin.com/in/michelle-de-araujo-santana/" target="_blank"><img
-                                src="assets/linkedin.png" alt="linkedin"></a>
+                        <a href='https://github.com/asmih' target='_blank'><img src='assets/github.png'
+                                alt='github'></a>
+                        <a href='https://www.linkedin.com/in/michelle-de-araujo-santana/' target='_blank'><img
+                                src='assets/linkedin.png' alt='linkedin'></a>
                     </div>
                 </section>
         
-                <section class="cards-poly">
-                    <div class="filtro">
+                <section class='cards-poly'>
+                    <div class='filtro'>
                         <h3>Polyana Magalhães</h3>
                         <p>Aluna da 8º geração da Laboratória</p>
-                        <a href="https://github.com/polyanagm" target="_blank"><img src="assets/github.png" alt="github"></a>
-                        <a href="https://www.linkedin.com/in/polyanagm/" target="_blank"><img
-                                src="assets/linkedin.png" alt="linkedin"></a>
+                        <a href='https://github.com/polyanagm' target='_blank'><img src='assets/github.png' alt='github'></a>
+                        <a href='https://www.linkedin.com/in/polyanagm/' target='_blank'><img
+                                src='assets/linkedin.png' alt='linkedin'></a>
                     </div>
                 </section>
         
